470e71af48b9b9609db03ae428cec257c49fe32d
gkrkdals <hagangmin@gmail.com>
2024-09-02 07:20:07 +0900

    same site 설정 변경

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
   ): Promise<Customer> {
     const token = await this.authService.signIn(signInDto.id);
     res.setHeader("Authorization", `Bearer ${token.access_token}`)
-    res.cookie("jwt", token.access_token, { httpOnly: true });
+    res.cookie("jwt", token.access_token, { httpOnly: true, sameSite: "none" });
 
     return token.payload
   }
