435a801469fca23ecd87e1984312a951932909ab
gkrkdals <hagangmin@gmail.com>
2024-09-02 07:24:27 +0900

    same site 설정 변경

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
   ): Promise<Customer> {
     const token = await this.authService.signIn(signInDto.id);
     res.setHeader("Authorization", `Bearer ${token.access_token}`)
-    res.cookie("jwt", token.access_token, { httpOnly: true, domain: 'yeonsu.kr' });
+    res.cookie("jwt", token.access_token, { httpOnly: true });
 
     return token.payload
   }
